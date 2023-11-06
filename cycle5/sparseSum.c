fd92de5d5230f8c5ab5f681c0f3bab4fa4bd7892
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-06 11:07:38 +0530

    Update sparseSum.c

--- cycle5/sparseSum.c
+++ cycle5/sparseSum.c
@@ -1,74 +1,94 @@
-void sum(int mat1[100][3],int mat2[100][3])
+#include<stdio.h>
+#include<stdlib.h>
+void main()
 {
-    int tr1,tr2,n=0,mat[100][3],i,j,k;
-    
-    if((mat1[0][0]==mat2[0][0]) && (mat1[0][1]==mat2[0][1]))
-    {
-        
-        tr1=mat1[0][2];
-        tr2=mat2[0][2];
-        i=1;
-        j=1;
-        k=1;
-        n=tr1+tr2;
-        while((i<=tr1) &&(j<=tr2))
-        {
-            if((mat1[i][0]==mat2[j][0]) &&(mat1[i][1]==mat2[j][1]))
-            {
-                mat[k][0]=mat1[i][0];
-                mat[k][1]=mat1[i][1];
-                mat[k][2]=mat1[i][2]+mat2[j][2];
-                i++;j++;k++;
-            }
-            
-               else if(mat1[i][0]<mat2[j][0])
-                {
-                    mat[k][0]=mat1[i][0];
-                mat[k][1]=mat1[i][1];
-                mat[k][2]=mat1[i][2];
-                i++;k++;
-                }
-                else
-                {
-                     mat[k][0]=mat2[j][0];
-                mat[k][1]=mat2[j][1];
-                mat[k][2]=mat2[j][2];
-                j++;k++;
-                }
-                       
-                   }
-        while(i<=tr1)
-        {
-           mat[k][0]=mat1[i][0];
-                mat[k][1]=mat1[i][1];
-                mat[k][2]=mat1[i][2];
-                i++;k++; 
-        }
-        while(j<=tr2)
-        {
-             mat[k][0]=mat2[j][0];
-                mat[k][1]=mat2[j][1];
-                mat[k][2]=mat2[j][2];
-                j++;k++;
-        }
-        mat[0][0]=mat1[0][0];
-        mat[0][1]=mat1[0][1];
-        mat[0][2]=k-1;
-        printf("\nThe sum is:\n");
-         for(i=0;i<k;i++)
-    {
-        printf("\n");
-        for(j=0;j<3;j++)
-        {
-            
-            printf("%d  ",mat[i][j]);
-        }
-    }
-    }
-    else
-    {
-        printf("The matrix is incompatible for addition\n");
-    }
+	int a[10][10],b[10][10],c[10][10],m1,n1,m2,n2,i,j,size1m=0,size1n=0,size2m=0,size2n=0;
+	printf("Enter rows and columns of 1st matrix : ");
+	scanf("%d%d",&m1,&n1);
+	printf("Enter rows and columns of 2nd matrix : ");
+	scanf("%d%d",&m2,&n2);
+	printf("Enter the elements of 1st matrix :");
+	for(i=0;i<m1;i++)
+	{
+		for(j=0;j<n1;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Enter the elements of 2nd matrix :");
+	for(i=0;i<m2;i++)
+	{
+		for(j=0;j<n2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	printf("\nFIRST MATRIX \n");
+	for(i=0;i<m1;i++)
+	{
+		for(j=0;j<n1;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nSECOND MATRIX \n");
+	for(i=0;i<m2;i++)
+	{
+		for(j=0;j<n2;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n\nCheck whether both the matrices are sparse matrices : ");
+	for(i=0;i<m1;i++)
+	{
+		for(j=0;j<n1;j++)
+		{
+			if(a[i][j]!=0)
+				size1m++;
+			else
+				size1n++;
+		}
+	}
+	printf("\nThe no of zero elements : %d",size1n);
+	printf("\nThe no of non-zero elements : %d",size1m);
+	printf("\n\n");
+	for(i=0;i<m2;i++)
+	{
+		for(j=0;j<n2;j++)
+		{
+			if(a[i][j]!=0)
+				size2m++;
+			else
+				size2n++;
+		}
+	}
+	printf("The no of zero elements : %d\n",size2n);
+	printf("The no of non-zero elements : %d\n",size2m);
+	if(size1n>size1m&&size2n>size2m)
+		printf("They are sparse matrices\n");
+	else
+	{
+		printf("They are not sparse matrices\n");
+		exit(0);
+	}
+	printf("Sum of matrices : \n");
+	for(i=0;i<m1;i++)
+	{
+		for(j=0;j<n1;j++)
+		{
+			c[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	for(i=0;i<m1;i++)
+	{
+		for(j=0;j<n1;j++)
+		{
+			printf("%d\t",c[i][j]);
+		}
+		printf("\n");
+	}
 }
-
 
