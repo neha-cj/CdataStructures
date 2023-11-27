901c82536ff2b7fbeafc9da322941ffac088bbe9
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-27 11:15:13 +0530

    Update 1dll.c
    create a ddl with n elements insertion at the end

--- cycle9/1dll.c
+++ cycle9/1dll.c
@@ -1,61 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* next;
-    struct Node* prev;
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *prev,*next;
 };
 
-struct Node* insertAtEnd(struct Node* head, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* last = head;
 
-    newNode->data = data;
-    newNode->next = NULL;
+struct node *newnode,*head,*temp;
 
-    if (head == NULL) {
-        newNode->prev = NULL;
-        return newNode;
-    }
-
-    while (last->next != NULL) {
-        last = last->next;
-    }
-
-    last->next = newNode;
-    newNode->prev = last;
-
-    return head;
+int main()
+{
+	int i,n;
+	head = NULL;
+	printf("Enter number of elements: ");
+	scanf("%d",&n);
+	printf("Enter the elements: ");
+	for(i=0;i<n;i++)
+	{
+		newnode = (struct node*)malloc(sizeof(struct node));
+		scanf("%d",&newnode->data);
+		newnode->prev = NULL;
+		newnode->next = NULL;
+		if(head == NULL)
+		{
+			head=newnode;
+			temp=newnode;
+		}
+		else
+		{
+			temp->next = newnode;
+			newnode->prev = temp;
+			temp = newnode;
+		}
+	}
+	printf("Created Linked List is:\n");
+	temp = head;
+	
+	while(temp != NULL)
+	{
+		printf("%d\t",temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+	return 0;
 }
-void traverse(struct Node* head) {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d<--->", temp->data);
-        temp = temp->next;
-    }
-    printf("NULL\n");
-}
-
-int main() {
-    struct Node* head = NULL;
-    /*int n;
-    printf("enter the number of elements you want to enter in linked list");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-    	printf("enter element\n");
-    	scanf("%d",&)*/
-
-    head = insertAtEnd(head, 10);
-    head = insertAtEnd(head, 20);
-    head = insertAtEnd(head, 30);
-    head = insertAtEnd(head, 40);
-
-    printf("Doubly Linked List: ");
-    traverse(head);
-
- 
-
-    return 0;
-}
-
