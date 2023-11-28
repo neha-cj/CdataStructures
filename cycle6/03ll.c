cfa5c15aa163a76263b794df01f0caa09281e466
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-28 22:48:20 +0530

    Update 03ll.c

--- cycle6/03ll.c
+++ cycle6/03ll.c
@@ -1,104 +1,108 @@
-//Given a node data, insert a new node after it.
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a node structure
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to add a new node after a given node
-void insertAfter(struct Node* prevNode, int newData) {
-    if (prevNode == NULL) {
-        printf("Previous node cannot be NULL.\n");
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertBefore(struct Node** head, struct Node* targetNode, int data) {
+    if (*head == NULL || targetNode == NULL) {
+        printf("Invalid parameters.\n");
         return;
     }
 
-    // Allocate memory for the new node
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* newNode = createNode(data);
 
-    // Set the data of the new node
-    newNode->data = newData;
+    if (*head == targetNode) {
+        newNode->next = *head;
+        *head = newNode;
+    } else {
+        struct Node* current = *head;
+        while (current != NULL && current->next != targetNode) {
+            current = current->next;
+        }
 
-    // Set the next of the new node to the next of the previous node
-    newNode->next = prevNode->next;
+        if (current == NULL) {
+            printf("Target node not found in the list.\n");
+            free(newNode);
+            return;
+        }
 
-    // Set the next of the previous node to the new node
-    prevNode->next = newNode;
-}
-
-// Function to print the linked list
-void printList(struct Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
+        newNode->next = targetNode;
+        current->next = newNode;
     }
 }
 
-// Main function
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node* head) {
+    while (head != NULL) {
+        struct Node* nextNode = head->next;
+        free(head);
+        head = nextNode;
+    }
+}
+
 int main() {
-    // Initialize a linked list with some elements
     struct Node* head = NULL;
 
-    // Number of elements in the linked list
-    int n;
+    int n, element;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    // Add elements to the linked list
+    printf("Enter the elements:\n");
     for (int i = 0; i < n; i++) {
-        int data;
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &data);
-
-        // Add the node at the end
-        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-        newNode->data = data;
-        newNode->next = NULL;
-
+        scanf("%d", &element);
         if (head == NULL) {
-            head = newNode;
+            head = createNode(element);
         } else {
-            struct Node* last = head;
-            while (last->next != NULL) {
-                last = last->next;
+            struct Node* current = head;
+            while (current->next != NULL) {
+                current = current->next;
             }
-            last->next = newNode;
+            current->next = createNode(element);
         }
     }
 
-    // Print the original linked list
     printf("Original Linked List: ");
     printList(head);
-    printf("\n");
 
-    // User input for data value and insert a new node after it
-    int newData;
-    printf("Enter the data value after which you want to insert a new node: ");
-    scanf("%d", &newData);
+    int dataToInsert, nodeDataToInsertBefore;
+    printf("Enter the data to be inserted: ");
+    scanf("%d", &dataToInsert);
 
-    struct Node* currentNode = head;
-    while (currentNode != NULL && currentNode->data != newData) {
-        currentNode = currentNode->next;
+    printf("Enter the data of the node before which to insert: ");
+    scanf("%d", &nodeDataToInsertBefore);
+
+    struct Node* nodeToInsertBefore = head;
+    while (nodeToInsertBefore != NULL && nodeToInsertBefore->data != nodeDataToInsertBefore) {
+        nodeToInsertBefore = nodeToInsertBefore->next;
     }
 
-    if (currentNode == NULL) {
-        printf("Node with data %d not found in the linked list.\n", newData);
-    } else {
-        // Insert a new node after the found node
-        int newElement;
-        printf("Enter the data value for the new node: ");
-        scanf("%d", &newElement);
-        insertAfter(currentNode, newElement);
+    insertBefore(&head, nodeToInsertBefore, dataToInsert);
 
-        // Print the updated linked list
-        printf("Updated Linked List: ");
-        printList(head);
-        printf("\n");
-    }
+    printf("Linked List after Insertion: ");
+    printList(head);
+
+    freeList(head);
 
     return 0;
 }
-
