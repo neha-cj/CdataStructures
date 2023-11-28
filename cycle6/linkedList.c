c65ebe91955be670900804a908dc705fdcbc4019
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-28 07:27:27 +0530

    Update linkedList.c

--- cycle6/linkedList.c
+++ cycle6/linkedList.c
@@ -1,71 +1,70 @@
+//Create a linked list with n elements by adding elements at the end
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure for doubly linked list
-struct Node {
-    int data;
-    struct Node* prev;
-    struct Node* next;
+struct Node{
+	int data;
+	struct Node*next;
 };
 
-// Function to insert a node at the end of the doubly linked list
-void insertEnd(struct Node** head, int data) {
+
+// Function to insert a node at the end of a linked list
+
+struct Node* insertAtEnd(struct Node* head, int newData) {
+    // Create a new node
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
+    newNode->data = newData;
     newNode->next = NULL;
 
-    if (*head == NULL) {
-        newNode->prev = NULL;
-        *head = newNode;
-        return;
+    // If the list is empty, make the new node the head
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        // Traverse to the end of the list
+        struct Node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+
+        // Insert the new node at the end
+        current->next = newNode;
     }
 
-    struct Node* last = *head;
-    while (last->next != NULL) {
-        last = last->next;
-    }
-
-    last->next = newNode;
-    newNode->prev = last;
+    // Return the head of the updated list
+    return head;
 }
 
-// Function to print the doubly linked list
+// Function to print the linked list
 void printList(struct Node* head) {
-    while (head != NULL) {
-        printf("%d <-> ", head->data);
-        head = head->next;
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d--> ", current->data);
+        current = current->next;
     }
     printf("NULL\n");
 }
 
-// Function to create a doubly linked list with n elements
-struct Node* createDoublyLinkedList(int n) {
+int main() {
+    // Initialize an empty linked list
     struct Node* head = NULL;
-
-    for (int i = 0; i < n; ++i) {
-        int data;
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &data);
-        insertEnd(&head, data);
+    
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    
+    for(int i=0; i<n;i++){
+    	int data;
+    	printf("enter element %d:",i+1);
+    	scanf("%d",&data);
+    	head = insertAtEnd(head, data);
     }
 
-    return head;
-}
+    
+    
 
-// Example usage
-int main() {
-    int n;
-
-    // Get the number of elements from the user
-    printf("Enter the number of elements in the doubly linked list: ");
-    scanf("%d", &n);
-
-    // Create a doubly linked list with n elements
-    struct Node* head = createDoublyLinkedList(n);
-
-    // Print the created list
-    printf("\nDoubly linked list with %d elements:\n", n);
+    // Print the linked list
+    printf("Linked List after Insertion at the End: ");
     printList(head);
-
-    return 0;
-}
+    
+    return(0);
+    }
