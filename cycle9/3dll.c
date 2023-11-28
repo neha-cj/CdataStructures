dd0534a09d9b68e34ad755d77b706e0d64b1ef36
Neha C J <119199146+neha-cj@users.noreply.github.com>
2023-11-28 07:28:14 +0530

    Update 3dll.c

--- cycle9/3dll.c
+++ cycle9/3dll.c
@@ -1,62 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure for doubly linked list
 struct Node {
     int data;
-    struct Node* next;
     struct Node* prev;
+    struct Node* next;
 };
-struct Node* insertAtBeginning(struct Node* head, int data) {
+
+// Function to insert a node at the end of the doubly linked list
+void insertEnd(struct Node** head, int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->next = head;
-    newNode->prev = NULL;
+    newNode->next = NULL;
 
-    if (head != NULL) {
-        head->prev = newNode;
+    if (*head == NULL) {
+        newNode->prev = NULL;
+        *head = newNode;
+        return;
     }
 
-    return newNode;
+    struct Node* last = *head;
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = newNode;
+    newNode->prev = last;
 }
 
-struct Node* insertAfter(struct Node* head, struct Node* prevNode, int data) {
+// Function to insert a new node after a node with a specific data value
+void insertAfter(struct Node* prevNode, int data) {
     if (prevNode == NULL) {
-        printf("Previous node cannot be NULL");
-        return head;
+        printf("Previous node cannot be NULL.\n");
+        return;
     }
 
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
+
     newNode->next = prevNode->next;
     newNode->prev = prevNode;
-    prevNode->next = newNode;
-
-    if (newNode->next != NULL) {
-        newNode->next->prev = newNode;
+    
+    if (prevNode->next != NULL) {
+        prevNode->next->prev = newNode;
     }
 
-    return head;
+    prevNode->next = newNode;
 }
-void traverse(struct Node* head) {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d<--->", temp->data);
-        temp = temp->next;
+
+// Function to print the doubly linked list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d <-> ", head->data);
+        head = head->next;
     }
     printf("NULL\n");
 }
 
+// Example usage
 int main() {
     struct Node* head = NULL;
 
-    head = insertAtBeginning(head, 5);
-    head = insertAtBeginning(head, 4);
-    head = insertAfter(head, head->next, 7);
-    head = insertAfter(head, head->next, 100);
-    head = insertAfter(head, head, 200);
-    printf("Doubly Linked List: ");
-    traverse(head);
+    // Add nodes to the doubly linked list
+    int nodesToAdd;
+    printf("Enter the number of nodes to add: ");
+    scanf("%d", &nodesToAdd);
 
-    
+    for (int i = 0; i < nodesToAdd; ++i) {
+        int data;
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertEnd(&head, data);
+    }
+
+    // Print the original list
+    printf("\nOriginal list:\n");
+    printList(head);
+
+    // Insert a new node after a node with a specific data value
+    int dataToInsert, dataAfter;
+    printf("\nEnter the data value to insert: ");
+    scanf("%d", &dataToInsert);
+
+    printf("Enter the data value after which to insert: ");
+    scanf("%d", &dataAfter);
+
+    struct Node* current = head;
+    while (current != NULL && current->data != dataAfter) {
+        current = current->next;
+    }
+
+    if (current != NULL) {
+        insertAfter(current, dataToInsert);
+    } else {
+        printf("Node with data %d not found.\n", dataAfter);
+    }
+
+    // Print the modified list
+    printf("\nList after insertion of node with data %d after node with data %d:\n", dataToInsert, dataAfter);
+    printList(head);
+
     return 0;
 }
